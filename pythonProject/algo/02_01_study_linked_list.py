3e42f31261889e9b651d8a1d281b6586a20a27c8
miseong Shin <miseong1028@gmail.com>
2021-01-19 18:13:29 +0900

    linked list

---
+++ pythonProject/algo/02_01_study_linked_list.py
@@ -0,0 +1,12 @@
+#[3]->[4]
+#data, next
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+node = Node(3)
+first_node = Node(4)
+node.next = first_node
+
+print(node.data)
